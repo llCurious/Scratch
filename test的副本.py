d3e6039209924fcaafd0995e00c6c43064fdcad0
吴豪奇 <1427104424@qq.com>
2017-03-08 21:12:33 +0800

    test ixia

---
+++ test的副本.py
@@ -0,0 +1,36 @@
+# -*- coding: UTF-8 -*-
+print "Hello, Python!";
+
+name="jack";
+home="jack";
+
+Money = 2000
+def AddMoney():
+   # 想改正代码就取消以下注释:
+   global Money
+   Money = Money + 1
+ 
+print Money
+AddMoney()
+print Money
+
+if(name is "jack"):
+	print "xiang deng de";
+	print "second"
+else:
+	print "no"
+
+total1=100; #quan ju bian liang
+num=raw_input("Enter a number!\n");
+print num;
+
+def sum(arg1,arg2):
+	#return the sum of two parameters
+	print total1;
+	total=arg1+arg2;
+	print "jubu: ",total
+	return total;
+
+xs=sum(10,20);
+print xs;
+print total1;
